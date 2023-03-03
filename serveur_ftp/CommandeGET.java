3d4a25c5c1d3e8bd109ebc1e1dd8129f374e0c18
abdelaziz-amil <abdelaziz.amil@etudiant.univ-brest.fr>
2023-03-03 17:08:37 +0100

    GET et PWD : TODO test GET et ajuster

--- serveur_ftp/CommandeGET.java
+++ serveur_ftp/CommandeGET.java
@@ -1,4 +1,4 @@
-import java.io.PrintStream;
+import java.io.*;
 
 public class CommandeGET extends Commande {
 	
@@ -7,7 +7,24 @@ public class CommandeGET extends Commande {
 	}
 
 	public void execute() {
+		try{
+			File file = new File(commandeArgs[0]);
+			String src = file.getAbsolutePath();
+			String dest = super.commandeArgs[1];
+			FileInputStream fis = new FileInputStream(src);
+			FileOutputStream fos = new FileOutputStream(dest);
+			byte[] buffer = new byte[4096];
+			int nbLecture;
+			while((nbLecture = fis.read(buffer)) != -1){
+				fos.write(buffer, 0, nbLecture);
+			}
 
+			fis.close();
+			fos.close();
+			ps.println("Transfert terminé");
+		}catch(Exception e){
+			System.out.println("Transfert échoué");
+		}
 	}
 
 }
