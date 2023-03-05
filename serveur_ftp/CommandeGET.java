f2ff3d74088c1655d5e2ab01209f0e8cfd17595c
abdelaziz-amil <abdelaziz.amil@etudiant.univ-brest.fr>
2023-03-05 10:01:42 +0100

    CommandeGET à tester

--- serveur_ftp/CommandeGET.java
+++ serveur_ftp/CommandeGET.java
@@ -1,28 +1,31 @@
 import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.channels.SocketChannel;
 import java.sql.ClientInfoStatus;
 
 public class CommandeGET extends Commande {
-	
+
 	public CommandeGET(PrintStream ps, String commandeStr) {
 		super(ps, commandeStr);
 	}
 
 	public void execute() {
-		try{
-			File file = new File(commandeArgs[0]);
-			String src = file.getAbsolutePath();
-			String dest = src + "/../" + "Client/";
-			FileInputStream fis = new FileInputStream(src);
-			FileOutputStream fos = new FileOutputStream(dest);
-			byte[] buffer = new byte[4096];
-			int nbLecture;
-			while((nbLecture = fis.read(buffer)) != -1){
-				fos.write(buffer, 0, nbLecture);
-			}
+		try(ServerSocket socketTransfer = new ServerSocket(4000)){
+			Socket socketCli = socketTransfer.accept();
 
+			OutputStream out = socketCli.getOutputStream();
+			InputStream fis = new FileInputStream(CommandExecutor.addPath(CommandExecutor.currentPath, commandeArgs[0]));
+			byte[] buffer = new byte[1024];
+			int nbOctetsLus;
+			while((nbOctetsLus = fis.read(buffer)) > 0){
+				out.write(buffer, 0, nbOctetsLus);
+				ps.println("1 " + nbOctetsLus + " octets envoyés");
+			}
+			out.close();
 			fis.close();
-			fos.close();
-			ps.println("Transfert terminé");
+			socketCli.close();
+
 		}catch(Exception e){
 			System.out.println("Transfert échoué");
 		}
