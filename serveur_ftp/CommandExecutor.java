8375513367ce3fcc602d5ba1f1e9f6bfc0de60dd
abdelaziz-amil <abdelaziz.amil@etudiant.univ-brest.fr>
2023-03-08 20:03:49 +0100

    Changes by darwh

--- serveur_ftp/CommandExecutor.java
+++ serveur_ftp/CommandExecutor.java
@@ -3,82 +3,127 @@ import java.io.PrintStream;
 
 public class CommandExecutor {
 
-    public static boolean userOk = false;
-    public static boolean pwOk = false;
+    public boolean userOk;
+    public boolean pwOk;
 
-    static File file = new File(".");
-    static String pathToAdd = addPath("serveur_ftp/", "root");
-    static String rootPath = addPath(file.getAbsoluteFile().toString().replace(".", ""), pathToAdd).replace("\\\\", "\\");
-    static String currentPath = rootPath;
+    File file;
+    String pathToAdd;
 
-    static String currentUser;
+    String rootPath;
+    String currentPath;
+    String currentUser;
 
-    public static void executeCommande(PrintStream ps, String commande) {
+    public CommandExecutor(){
+        this.userOk = false;
+        this.pwOk = false;
+        file = new File(".");
+        pathToAdd = addPath("serveur_ftp", "root");
+        rootPath = addPath(file.getAbsoluteFile().toString().
+                replace(".", ""), pathToAdd).replace("\\\\", "\\");
+        currentPath = rootPath;
+        currentUser = "";
+    }
+
+    public void executeCommande(PrintStream ps, String commande) {
         if (userOk && pwOk) {
             // Changer de repertoire. Un (..) permet de revenir au repertoire superieur
             if (commande.split(" ")[0].equals("cd")) {
-                (new CommandeCD(ps, commande)).execute();
+                (new CommandeCD(ps, commande, this)).execute();
             }
 
             // Télécharger un fichier
             if (commande.split(" ")[0].equals("get")) {
-                (new CommandeGET(ps, commande)).execute();
+                (new CommandeGET(ps, commande, this)).execute();
             }
 
             // Afficher la liste des fichiers et des dossiers du repertoire courant
             if (commande.split(" ")[0].equals("ls")) {
-                (new CommandeLS(ps, commande)).execute();
+                (new CommandeLS(ps, commande, this)).execute();
             }
 
             // Afficher le repertoire courant
             if (commande.split(" ")[0].equals("pwd")) {
-                (new CommandePWD(ps, commande)).execute();
+                (new CommandePWD(ps, commande, this)).execute();
             }
 
             // Envoyer (uploader) un fichier
             if (commande.split(" ")[0].equals("stor")) {
-                (new CommandeSTOR(ps, commande)).execute();
+                (new CommandeSTOR(ps, commande, this)).execute();
             }
 
             // Créer un dossier
             if (commande.split(" ")[0].equals("mkdir")) {
-                (new CommandeMKDIR(ps, commande)).execute();
+                (new CommandeMKDIR(ps, commande, this)).execute();
             }
 
             // Supprimer un dossier vide
             if (commande.split(" ")[0].equals("rmdir")) {
-                (new CommandeRMDIR(ps, commande)).execute();
+                (new CommandeRMDIR(ps, commande, this)).execute();
             }
         } else {
             if (commande.split(" ")[0].equals("pass") || commande.split(" ")[0].equals("user")) {
                 // Le mot de passe pour l'authentification
                 if (commande.split(" ")[0].equals("pass")) {
-                    (new CommandePASS(ps, commande)).execute();
+                    (new CommandePASS(ps, commande, this)).execute();
                 }
 
                 // Le login pour l'authentification
                 if (commande.split(" ")[0].equals("user")) {
-                    (new CommandeUSER(ps, commande)).execute();
+                    (new CommandeUSER(ps, commande, this)).execute();
                 }
             } else
                 ps.println("2 Vous n'êtes pas connecté !");
         }
     }
 
-    public static String addPath(String path, String add) {
+    public static String getFolderSeperator() {
         String OS = System.getProperty("os.name").toLowerCase();
-
-        if (OS.contains("win")) {
-            return path + add + "\\";
-        } else {
-            return path + add + "/";
-        }
+        return (OS.contains("win")) ? "\\" : "/";
     }
 
-    public static void reset() {
+    public static String addPath(String path, String add) {
+
+        String folderSeparator = getFolderSeperator();
+
+        // Avoir path et add bien formatté afin qu'ils puissent être ajouté entre eux
+        boolean formatted = false;
+        while (!formatted) {
+            if (path.endsWith(folderSeparator)) path = path.substring(0, path.length() - 1);
+            if (add.startsWith(folderSeparator)) add = add.substring(1);
+            if (add.endsWith(folderSeparator)) add = add.substring(0, add.length() - 1);
+
+            if (!path.endsWith(folderSeparator) || !add.startsWith(folderSeparator)) {
+                formatted = true;
+            }
+        }
+
+        return path + folderSeparator + add;
+    }
+
+    public void goBackOneDirectory() {
+        String folderSeperator = getFolderSeperator();
+
+        currentPath = currentPath.substring(0, currentPath.lastIndexOf(folderSeperator));
+    }
+
+    public void reset() {
         userOk = false;
         pwOk = false;
         currentPath = rootPath;
         currentUser = "";
+    }
+
+    public String findSubDirectory(String dir) {
+        // On veut aller dans un subdirectory
+        File file = new File(currentPath);
+        String[] directories = file.list((current, name) -> new File(current, name).isDirectory());
+        if (directories != null) {
+            for (String directory : directories) {
+                if (dir.equals(directory)) {
+                    return directory;
+                }
+            }
+        }
+        return "";
     }
 }
